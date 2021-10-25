0086e303e6bc55c08911c26ca0647598d561091e
denis <denis@LAPTOP-7MNEL9F1>
2021-10-25 21:50:39 +1000

    Добавьте файлы проекта.

---
+++ ОАИП/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ОАИП
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random a = new Random();
+            int x = a.Next(100, 1000);
+
+            int b = 0;
+
+            while (b != x)
+            {
+                Console.WriteLine("Введите переменную b");
+                int.TryParse(Console.ReadLine(), out b);
+
+                if (b > x)
+                {
+                    Console.WriteLine("Много");
+                }
+
+                else if (b < x)
+                {
+                    Console.WriteLine("Мало");
+                }
+
+                else
+                {
+                    Console.WriteLine("Угадал");
+                }
+            }
+        }
+    }
+}
